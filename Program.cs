fc73204b0e6f2867f9a0f8cc0190f8141ab9906b
DhanashriKhade125 <coder.dhanashrikhade12@gmail.com>
2022-12-30 18:10:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Radius");
+            double rad = Convert.ToDouble(Console.ReadLine());
+            double area = 3.14 * rad * rad;
+            Console.WriteLine($"Area of circle is: {area}");
+        }
+    }
+    }
+
